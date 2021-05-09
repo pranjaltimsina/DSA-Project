09fcda7a5603411b8309fe277453ef5b3ba6f000
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-05-09 12:03:25 +0530

    fix: linted ugly code

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from curses import wrapper
 NOTE: All coordinates are in the format (y, x) because that's how curses works)
 """
 
+
 def validate_key(c: int):
     invalids = [
             10,
             263,
             262,
-            360, 
+            360,
             331,
             339,
             330,
@@ -33,6 +34,7 @@ def validate_key(c: int):
     else:
         return True
 
+
 def main(s):
     '''
     * s is the whole screen as an object
@@ -40,63 +42,63 @@ def main(s):
     * by accident
     '''
 
-    sh, sw = s.getmaxyx() # Get the height, and width of the terminal
+    sh, sw = s.getmaxyx()  # Get the height, and width of the terminal
 
-    s.clear() # Clear the terminal
-    curses.raw() # Get raw input from the keyboard 
-    curses.echo() # Echo the input keys to the screen
-    curses.nl() # Set enter to correspond to new line
+    s.clear()  # Clear the terminal
+    curses.raw()  # Get raw input from the keyboard
+    curses.echo()  # Echo the input keys to the screen
+    curses.nl()  # Set enter to correspond to new line
 
     # Create a title box with height 2 rows and width 100%, starting at 1, 0
     title_box = s.subwin(2, sw-7, 1, 3)
     title_box.addstr('Directory searching using Trie, fuzzysearch and DFS', curses.A_REVERSE)
-    title_box.chgat(-1, curses.A_REVERSE) # That sweet background on the title
+    title_box.chgat(-1, curses.A_REVERSE)  # That sweet background on the title
     title_box.hline(1, 0, '-', sw-7)
-    
-    # Create a search box of height 2 rows, width 100 at 3, 0 
+
+    # Create a search box of height 2 rows, width 100 at 3, 0
     search_box = s.subwin(2, sw, 3, 0)
-    search_box.addstr('   Search: ') 
-    search_box.hline(1, 3, '-', sw-7) 
+    search_box.addstr('   Search: ')
+    search_box.hline(1, 3, '-', sw-7)
 
     # The output box that covers the rest of the screen
     output_box = s.subwin(sh-5, sw-3, 5, 3)
     output_box.addstr('Results:\nTo find, you must seek')
-    
+
     # Instructions the bottom of the screen
     s.addstr(sh-1, 3, 'Start typing to search! Press <ESC> to exit.')
 
-    input_x = 11 # The x coordinate of the cursor 
-    full_string = "" # full_string is the search query
-    
+    input_x = 11  # The x coordinate of the cursor
+    full_string = ""  # full_string is the search query
+
     # Store the output here, edit line 86 to format things appropriately
     matches = []
-    
+
     # Main loop
     while True:
         # Get a character from the keyboard
         c = s.getch(3, input_x)
 
         if (c in [263, 127] and not input_x == 11):
-            # Check if backspace and not empty string 
-            input_x -=1 # Decrement cursors x-coordinate
-            full_string = full_string[:-1] # Remove last char of search query
-            s.delch(3,input_x) # Remove the character from the screen
-            output_box.clear() # Clear the output box 
+            # Check if backspace and not empty string
+            input_x -= 1  # Decrement cursors x-coordinate
+            full_string = full_string[:-1]  # Remove last char of search query
+            s.delch(3, input_x)  # Remove the character from the screen
+            output_box.clear()  # Clear the output box
         elif not validate_key(c):
             continue
 
-        elif not chr(c) == "\n":  
+        elif not chr(c) == "\n":
             # Add the chr to the search query and increment cursor position
             full_string += chr(c)
             input_x += 1
-        
+
         """
-            TODO: fuzzy search the dirs and store the result in matches 
+            TODO: fuzzy search the dirs and store the result in matches
         """
 
         if (not full_string == "" and not matches == []):
             # Clear the output box and add the matches
-            output_box.clear() 
+            output_box.clear()
             for match in matches:
                 output_box.addstr(f'{match[0]:>4} | {match[1]}\n')
 
@@ -110,18 +112,19 @@ def main(s):
             output_box.clear()
             output_box.addstr(f"{full_string}\n")
             output_box.addstr('What you seek for lies beyond the realms of possibility')
-        
-        # refreesh all boxes 
+
+        # refreesh all boxes
         search_box.refresh()
         output_box.refresh()
         s.refresh()
 
-        # since everything cleared, the message at bottom needs to be written 
+        # since everything cleared, the message at bottom needs to be written
         s.addstr(sh-1, 3, 'Start typing to search! Press <ESC> twice to exit.')
 
         if (c == 27):
             # Quit if <ESC> is pressed
-           curses.endwin()
-           break
+            curses.endwin()
+            break
+
 
 wrapper(main)
